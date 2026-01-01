4863c5580cb0e1187279b5e7a322f396d4dae89e
SerenaJewels <serenaqhuang@gmail.com>
2026-01-01 14:31:26 -0500

    live server

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+const connectLiveReload = require("connect-livereload");
+
+liveReloadServer.watch(path.join(__dirname, 'public'))
+
+app.use(connectLiveReload())
+
+app.use(express.static(__dirname));
 
 // setup routes
 router.get('/', function(req, res) {
@@ -13,3 +22,10 @@ router.get('/', function(req, res) {
 app.use('/', router);
 app.listen(process.env.port || 3000);
 console.log('Running at Port 3000');
+
+//refreshing
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+})
